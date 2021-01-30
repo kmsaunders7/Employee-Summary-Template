6e92227c85848265cac234e547157bc4aaa5bdfb
Kristin Saunders <kmsaunders7@gmail.com>
2021-01-30 17:38:55 -0600

    created more functions in app.js

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { writeFile } = require("fs/promises");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//start with empty array to hold employees
 
 const employees = []
 
-const employeePrompts = [
+const employeeInfo = [
     {
         type: 'input',
         name: 'name',
@@ -42,22 +42,109 @@ const employeePrompts = [
     
 ]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function startQuestions () {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'continue',
+            message: 'Do you want to add another employee?',
+            choices: ['Yes', 'No']
+        }
+    ).then((response) => {
+        if (response.continue === 'Yes') {
+            selectEmployee()
+        } else {
+            writeFile()
+        }
+    }).catch(error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function selectEmployee() {
+    inquirer.prompt(employeeInfo).then((response) => {
+        if (response.title === 'Manager') {
+            createManager(response)
+        } else if (response.title === 'Engineer') {
+            createEngineer(response)
+        } else if (response.title === 'Intern') {
+            createIntern(response)
+        } else {
+            console.log('error')
+        }
+    })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function createManager (managerInfo) {
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the employees office phone number?'
+        }
+    ).then(async(response) => {
+        const {name, email, id} = managerInfo
+        let newManager = await new Manager (name, email, id, response.officeNumber)
+        employees.push(newManager)
+        //run main init function
+    }).catch(error => {
+        if(error) {
+            console.log(error)
+        }
+    })
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//function for engineer prompts (github)...
+function createEngineer(engineerInfo) {
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the employees github username?'
+        }
+    ).then(async(response) => {
+        const {name, email, id} = engineerInfo
+        let newEngineer = await new Engineer (name, email, id, response.gitHub)
+        employees.push(newEngineer)
+        //run main init function
+    }).catch(error => {
+        if(error) {
+            console.log(error)
+        }
+    })
+}
+
+//function for intern prompts (school)...
+function createIntern (internInfo) {
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'schoolName',
+            message: 'What school is the employee currently enrolled?'
+        }
+    ).then(async(response) => {
+        const {name, email, id} = internInfo
+        let newIntern = await new Intern (name, email, id, response.schoolName)
+        employees.push(newIntern)
+        //run main init function
+    }).catch(error => {
+        if(error) {
+            console.log(error)
+        }
+    })
+}
+
+// begins the entire set of questions(prompts)
+startQuestions();
+
+//calls the function to create the html file by putting the employee array into render function
+function createWebsite() {
+    fs.writeFileSync(outputPath, render(employees), function(error) {
+        if (error) {
+            console.log('error')
+        }
+    })
+    console.log("Website has been created with employee information!")
+}
